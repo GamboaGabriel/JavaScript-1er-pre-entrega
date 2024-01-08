4eb10a08d2abff59a3611c7e1dcd5796be88f88d
Maximiliano Gamboa <maxisjrc@gmail.com>
2024-01-08 15:10:04 -0300

    1er pre entrega finalizada

--- script.js
+++ script.js
@@ -126,16 +126,21 @@ function nombreFuncion(){
 function calcular() {
     let precio = parseFloat(document.getElementById("precio").value);
     let cuotas = parseInt(document.getElementById("cuotas").value);
+    let aplicarDescuento = document.getElementById("descuento").value.toLowerCase() === 'si';
     
     if (isNaN(precio) || isNaN(cuotas) || precio <= 0 || cuotas <= 0) {
         alert("Por favor, ingrese valores válidos.");
     }
     
-    let iva = precio * 0.21;
-    let costoTotalConImpuestos = precio + iva;
+    let descuento = aplicarDescuento ? precio * 0.20 : 0;
+    let precioConDescuento = precio - descuento;
+    let iva = precioConDescuento * 0.21;
+    let costoTotalConImpuestos = precioConDescuento + iva;
     let montoPorCuota = costoTotalConImpuestos / cuotas;
 
     document.getElementById("costo-total").innerText = costoTotalConImpuestos.toFixed(2);
     document.getElementById("costo-por-cuota").innerText = montoPorCuota.toFixed(2);
+
+    
 }
 
