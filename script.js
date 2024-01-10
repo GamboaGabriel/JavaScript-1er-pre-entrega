17c0bbd4f8422e1826d634c6215a2eb186babb70
Maximiliano Gamboa <maxisjrc@gmail.com>
2024-01-10 11:42:14 -0300

    Revision

--- script.js
+++ script.js
@@ -3,16 +3,38 @@ function calcular() {
     let cuotas = parseInt(document.getElementById("cuotas").value);
     let aplicarDescuento = document.getElementById("descuento").value.toLowerCase() === 'si';
     
-    if (isNaN(precio) || isNaN(cuotas) || precio <= 0 || cuotas <= 0) {
+    //Si el precio o las cuotas del producto son menores o iguales a "0", se pone en funcionamiento el if //
+    if (precio <= 0 || cuotas <= 0) {
         alert("Por favor, ingrese valores válidos.");
+    //Si el numero de cuotas excede las 12 cuotas hay otro alert //
+    }else if (cuotas > 12){
+        alert("Por el momento solo podemos hacer el plan de Hasta 12 cuotas. Disculpe las molestias");
+        return;
     }
+
+    if (![3, 6, 12].includes(cuotas)) {
+        alert("Solo aceptamos pagos en 3, 6 ó 12 cuotas. Por favor, seleccione una de estas opciones.");
+        return;
+    }
+
     
+    //Variables
     let descuento = aplicarDescuento ? precio * 0.20 : 0;
     let precioConDescuento = precio - descuento;
     let iva = precioConDescuento * 0.21;
     let costoTotalConImpuestos = precioConDescuento + iva;
     let montoPorCuota = costoTotalConImpuestos / cuotas;
 
+    //Ciclo que se repite si la cuota es igual a 1
+    let unaCuota = 1;
+    while (unaCuota == cuotas) {
+        let montoPorCuota = costoTotalConImpuestos / cuotas;
+        alert(`Cuota ${unaCuota}: $${montoPorCuota.toFixed(2)}
+${"Se va a cobrar impuestos aunque sea una sola cuota, y no aplica el descuento del 20%. Gracias!!"}`);
+        unaCuota++;
+
+    }
+    //Evento para mostrar el resultado en numeros con dos decimales
     document.getElementById("costo-total").innerText = costoTotalConImpuestos.toFixed(2);
     document.getElementById("costo-por-cuota").innerText = montoPorCuota.toFixed(2);
 
